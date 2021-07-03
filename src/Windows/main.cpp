991c4862bbafbd592d6317e57da24e19ed94fe4a
Jean-Marc Pelletier <jmp@nzu.ac.jp>
2021-07-03 12:01:56 +0900

    Added license info.

--- src/Windows/main.cpp
+++ src/Windows/main.cpp
@@ -1,10 +1,11 @@
-// Dear ImGui: standalone example application for DirectX 12
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
-// Read online: https://github.com/ocornut/imgui/tree/master/docs
-
-// Important: to compile on 32-bit systems, the DirectX12 backend requires code to be compiled with '#define ImTextureID ImU64'.
-// This is because we need ImTextureID to carry a 64-bit value and by default ImTextureID is defined as void*.
-// This define is set in the example .vcxproj file and need to be replicated in your app or by adding it to your imconfig.h file.
+/* 
+* This file is largely copied from the DirectX12 example
+* from Dear ImGui, which is distributed according to the 
+* MIT license.
+* https://github.com/ocornut/imgui
+* https://github.com/ocornut/imgui/blob/master/examples/example_win32_directx12/main.cpp
+* https://github.com/ocornut/imgui/blob/master/LICENSE.txt
+*/
 
 #include "../Application.hpp"
 
